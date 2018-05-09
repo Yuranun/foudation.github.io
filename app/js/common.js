a7a5cd668b3755517b12bd53036ada2af5f055bc
Yura-Sachnez <kyrizan96@gmail.com>
2018-05-09 21:19:44 +0300

    Add files via upload

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	 var mySwiper = new Swiper ('.swiper-container', {
+	 	
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+});
